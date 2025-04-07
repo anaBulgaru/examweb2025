7dcce72c05931afd0080b6b595811ddb41f4b359
BULGARU ANA MARIA <ana.bulgaru@student.eafc-sudlux.be>
2025-04-07 23:28:04 +0200

    js file

---
+++ menu.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+    if (window.innerWidth <= 768) {
+        console.log("Appareil portable détecté");
+      }
+      else {
+        console.log(window.innerWidth);
+        const menuElement = document.getElementById('menu');
+   
+        if (menuElement) {
+            menu.classList.toggle("hidden");
+        }
+      }
+
+    
+  };
